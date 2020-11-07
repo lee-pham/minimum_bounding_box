9df8c49deaa0c6092baf4766587ee2e486bc4574
Lee Pham <leepham@utexas.edu>
2020-11-07 00:25:43 -0500

    using enumerate was unnecessary since code was rewritten to handle any file name

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import glob
 import os
 from PIL import Image
 
-pngs = glob.glob(r'originals/*.png')
+pngs = glob.glob('originals/*.png')
 
-for i, png in enumerate(pngs):
+for png in pngs:
     im = Image.open(png)
     cropped_im = im.crop(im.convert('RGBa').getbbox())
-    cropped_im.save(f'cropped/{os.path.basename(png)}')
-    print(f'Converted {png} to cropped/{os.path.basename(png)}')
+    file_name = os.path.basename(png)
+    cropped_im.save(f'cropped/{file_name}')
+    print(f'Converted {png} to cropped/{file_name}')
